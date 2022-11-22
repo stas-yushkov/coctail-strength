db9fdf41692b818d4728720ad6163f275b36e2ab
stas-yushkov <stas.yushkov@gmail.com>
2022-11-22 22:31:17 +0200

    upd color, rename weights to volumes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const coctailsArr = [
-  // { name: 'віскі', weight: 90, strength: 40 },
-  // { name: 'вермут', weight: 30, strength: 16 },// result 34%
+  // { name: 'віскі', volume: 90, strength: 40 },
+  // { name: 'вермут', volume: 30, strength: 16 },// result 34%
 ];
 
 const strengthInputList = document.querySelectorAll('input.JS-strength-input');
@@ -13,30 +13,29 @@ const resultIngridientDivList = document.querySelectorAll('.ingridient-vis');
 
 const resultNumber = document.querySelector('p.JS-result-number');
 const resultText = document.querySelector('p.JS-result-text');
-// const resultButton = document.querySelector('button.JS-result-button');
 
 const resultCoctailDivHeight = resultCoctailDiv.clientHeight;
 
 const recalc = () => {
-  const initialWeight = 0;
+  const initialVolume = 0;
 
-  const alcoholWeight = coctailsArr.reduce(
-    (prev, curr, currIdx) => (prev + curr.weight * (coctailsArr[currIdx].strength / 100))
-    , initialWeight
+  const alcoholVolume = coctailsArr.reduce(
+    (prev, curr, currIdx) => (prev + curr.volume * (coctailsArr[currIdx].strength / 100))
+    , initialVolume
   );
 
-  const coctailWeight = coctailsArr.reduce(
-    (prev, curr) => (prev + curr.weight)
-    , initialWeight
+  const coctailVolume = coctailsArr.reduce(
+    (prev, curr) => (prev + curr.volume)
+    , initialVolume
   );
 
-  if (!coctailWeight) {
+  if (!coctailVolume) {
     resultNumber.textContent = '...';
     resultText.textContent = '... схоже, тут пусто ...';
     return;
   }
 
-  const coctailStrength = Math.round((alcoholWeight / coctailWeight) * 1000) / 10;
+  const coctailStrength = Math.round((alcoholVolume / coctailVolume) * 1000) / 10;
 
   if (coctailStrength === 0) {
     resultNumber.textContent = `${coctailStrength}%`;
@@ -52,11 +51,10 @@ const recalc = () => {
 
   resultNumber.textContent = `${coctailStrength}%`;
   resultText.textContent = 'та й таке )';
-  // resultText.textContent = `міцності`;
   return;
 }
 
-const debounce = (func, timeout = 300) => {
+const debounce = (func, timeout = 250) => {
   let timer = null;
 
   return () => {
@@ -68,60 +66,24 @@ const debounce = (func, timeout = 300) => {
 const debouncedRecalc = debounce(() => recalc());
 
 const redraw = () => {
-  const initialWeight = 0;
+  const initialVolume = 0;
 
-  // const alcoholWeight = coctailsArr.reduce(
-  //   (prev, curr, currIdx) => (prev + curr.weight * (coctailsArr[currIdx].strength / 100))
-  //   , initialWeight
-  // );
-
-  const coctailWeight = coctailsArr.reduce(
-    (prev, curr) => (prev + curr.weight)
-    , initialWeight
+  const coctailVolume = coctailsArr.reduce(
+    (prev, curr) => (prev + curr.volume)
+    , initialVolume
   );
 
-  // const coctailStrength = Math.round((alcoholWeight / coctailWeight) * 1000) / 10;
-
-  // resultIngridientDivList[idx].style.backgroundColor = 'red';
-  // resultIngridientDivList[idx].style.height = '10px';
-  // style.backgroundColor = "teal"
-  // console.log();
-  resultCoctailDivHeight
-
+  // TODO: choose color for alco & nonAlco
   // TODO: fix ingridient color?
 
   resultIngridientDivList.forEach((elem, idx) => {
+    const updatedHeight = (coctailsArr[idx].volume * resultCoctailDivHeight) / coctailVolume
 
-    // console.dir(elem);
-
-    elem.style.height = `${(coctailsArr[idx].weight * resultCoctailDivHeight) / coctailWeight}px`;
-    // console.log(Math.round(255 * (coctailsArr[idx].strength) / 100));
+    elem.style.height = `${updatedHeight}px`;
 
     const color = coctailsArr[idx].strength;
-    elem.style.backgroundColor = `rgb(20%, 10%, ${color}%)`;
-
-    // elem.classList.remove('display-none');
-    // elem.style.borderWidth = '2px';
-
-    if (!coctailsArr[idx].weight) {
-      // elem.style.borderWidth = '0';
-      // elem.classList.add('display-none');
-      // return
-    };
-
-    // const color = Math.round(255 * (coctailsArr[idx].strength) / 100);
-    // elem.style.backgroundColor = `rgb(0, 0, ${color})`;
+    elem.style.backgroundColor = `rgb(${color}%, 0% , ${100 - color}% )`;
   })
-
-  // if (!coctailStrength) {
-  //   return resultText.textContent = '... схоже, тут безалкогольний коктейль ...';
-  // }
-
-  // if (coctailStrength >= 90) {
-  //   return resultText.textContent = `${coctailStrength}% ... розірве в друзки ...`;
-  // }
-
-  // return resultText.textContent = `${coctailStrength}%`;
 }
 
 strengthLabelList.forEach((_, idx) => {
@@ -130,12 +92,12 @@ strengthLabelList.forEach((_, idx) => {
   if (!coctailsArr[idx]) {
     coctailsArr.push({
       strength: Number(strengthInputList[idx].value),
-      weight: Number(volumeInputList[idx].value),
+      volume: Number(volumeInputList[idx].value),
       name: coctailsArr[idx]?.name || idx
     })
   } else {
     coctailsArr[idx].strength = Number(strengthInputList[idx].value);
-    coctailsArr[idx].weight = Number(volumeInputList[idx].value);
+    coctailsArr[idx].volume = Number(volumeInputList[idx].value);
     coctailsArr[idx].name = coctailsArr[idx]?.name || idx;
   }
 })
@@ -153,11 +115,9 @@ document.addEventListener('input', e => {
   volumeInputList.forEach((item, idx) => {
     if (item === e.target) {
       volumeLabelList[idx].textContent = Number(e.target.value);
-      coctailsArr[idx].weight = Number(e.target.value);
+      coctailsArr[idx].volume = Number(e.target.value);
     }
   })
   redraw();
   debouncedRecalc();
 })
-
-// resultButton.addEventListener('click', recalc);
